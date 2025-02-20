0cb3eb225471a6ccc5b6ed0339a86cbdcf89e7a5
Azwad <azwad.sidrat@gmail.com>
2025-02-20 17:57:32 +0600

    Involution Memory efficient Implementation Added

--- New_Revisions/model.py
+++ New_Revisions/model.py
@@ -1,61 +1,57 @@
 import torch
 import torch.nn as nn
-from involution import Involution2d
+from inv import involution
+from torchsummary import summary
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+"""
+class involution(nn.Module):
 
+    def __init__(self,channels,kernel_size,stride):
+        super(involution, self).__init__()
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.channels = channels
+        reduction_ratio = 4
+        self.group_channels = 16
+        self.groups = self.channels // self.group_channels
+        self.conv1 = nn.Conv2d(in_channels=channels,out_channels=channels // reduction_ratio,kernel_size=1)
+        self.bn1 = nn.BatchNorm2d(channels // reduction_ratio)
+        self.relu = nn.ReLU()
+        self.conv2 = nn.Conv2d(in_channels=channels // reduction_ratio,out_channels=kernel_size**2 * self.groups,kernel_size=1,stride=1,)
+  
+        if stride > 1:
+            self.avgpool = nn.AvgPool2d(stride, stride)
+        self.unfold = nn.Unfold(kernel_size, 1, (kernel_size-1)//2, stride)
 
-
+    def forward(self, x):
+        weight = self.conv1(x if self.stride == 1 else self.avgpool(x))
+        weight = self.bn1(weight)
+        weight = self.relu(weight)
+        weight = self.conv2(weight)
+        b, c, h, w = weight.shape
+        weight = weight.view(b, self.groups, self.kernel_size**2, h, w).unsqueeze(2)
+        out = self.unfold(x).view(b, self.groups, self.group_channels, self.kernel_size**2, h, w)
+        out = (weight * out).sum(dim=3).view(b, self.channels, h, w)
+        return out
+"""
 
 class block(nn.Module):
-    def __init__(
-        self, in_channels, intermediate_channels, identity_downsample=None, stride=1 , type='inv' 
-    ):
+    def __init__(self, in_channels, intermediate_channels, identity_downsample=None, stride=1 , type='inv'):
         super().__init__()
         self.expansion = 4
-        self.conv1 = nn.Conv2d(
-            in_channels,
-            intermediate_channels,
-            kernel_size=1,
-            stride=1,
-            padding=0,
-            bias=False,
-        )
-        
-        
+        self.conv1 = nn.Conv2d(in_channels,intermediate_channels,kernel_size=1,stride=1,padding=0,bias=False)
         self.bn1 = nn.BatchNorm2d(intermediate_channels)
 
         if(type=="inv"):
-            self.conv2 = Involution2d(
-                intermediate_channels,
-                intermediate_channels,
-                kernel_size=(7,7),
-                stride=(stride,stride),
-                groups = 16,
-                reduce_ratio= 4
-            )
-        else:
-
-
-
-            self.conv2 = nn.Conv2d(
-                in_channels=intermediate_channels,
-                out_channels=intermediate_channels,
-                kernel_size=(3, 3),
-                stride=(stride,stride),
-                padding = 1,
-                groups=intermediate_channels  #Depthwise
-                )
-        self.bn2 = nn.BatchNorm2d(intermediate_channels)
-        self.conv3 = nn.Conv2d(
-            intermediate_channels,
-            intermediate_channels * self.expansion,
-            kernel_size=1,
-            stride=1,
-            padding=0,
-            bias=False,
-        )
+            self.conv2 = involution(intermediate_channels,kernel_size=7,stride=stride)
         
+        else:
+            self.conv2 = nn.Conv2d(in_channels=intermediate_channels,out_channels=intermediate_channels,kernel_size=(3, 3),stride=stride,padding = 1,groups=intermediate_channels)
+        
+        
+        self.bn2 = nn.BatchNorm2d(intermediate_channels)
+        self.conv3 = nn.Conv2d(intermediate_channels,intermediate_channels * self.expansion,kernel_size=1,stride=1,padding=0,bias=False,)
         self.bn3 = nn.BatchNorm2d(intermediate_channels * self.expansion)
         self.gelu = nn.GELU()
         self.identity_downsample = identity_downsample
@@ -99,7 +95,7 @@ class INN_DWConv(nn.Module):
             block, layers[0], intermediate_channels=64, stride=1 , type='inv'
         )
         self.layer2 = self._make_layer(
-            block, layers[1], intermediate_channels=128, stride=2 , type = 'inv'
+            block, layers[1], intermediate_channels=128, stride=1 , type = 'inv'
         )
         self.layer3 = self._make_layer(
             block, layers[2], intermediate_channels=256, stride=2 , type='conv'
@@ -172,5 +168,5 @@ def Stage_1_2(img_channel=1, num_classes=3):
 
 model = Stage_1_2(1,3)
 stage_1_2 = model.to(device)
-x = torch.ones(16,1,224,244).to(device)
-print(stage_1_2(x))
+
+summary(stage_1_2,(1,224,224))
